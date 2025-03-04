b4db39f1aeb468aa9257999f524f28832c9754a8
Vishal Srivastava <eth.vishal@gmail.com>
2025-03-04 23:24:08 +0530

    some ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="flex justify-center">
       <CardWithForm />
-      <div className="text-gray-500 text-sm font-bold absolute bottom-0">
+      {/* <div className="text-gray-500 text-sm font-bold absolute bottom-0">
         <p>Designed and developed by Vishal Srivastava.</p>
-      </div> 
+      </div>  */}
     </div>
   );
 }
--- src/ConverterWindow.tsx
+++ src/ConverterWindow.tsx
@@ -42,19 +42,19 @@ export function CardWithForm() {
       <div className="shadow-xl grid  min-w-[80vw] min-h-[25vh] md:min-h-[50vh] items-center ">
       
       {/* Title */}
-        <div className="bg-gradient-to-r to-purple-500 from-black 100%       
+        <div className="bg-gradient-to-r to-gray-600 from-black 100%       
          rounded-t-md   text-gray-200  md:pt-5 pt-2">
           <div className="text-2xl md:text-4xl text-center">Krutidev to Unicode</div>
-          <div className="flex space-x-3 p-2 ">
+          <div className="flex space-x-6 p-2 mt-2 ml-2">
+            <div>
+                <input className="mr-2" defaultChecked type="radio" id="Krutidev014/016" name="fontName" value="Krutidev014/016"
+                onChange={(e)=>{console.log(e.target.value);setFontName(e.target.value)}} />
+                <label htmlFor="Krutidev014/016">Krutidev_011/014/016</label>
+            </div>
             <div>
                 <input className="mr-2" type="radio" id="Krutidev010" name="fontName" value="Krutidev010" 
                 onChange={(e)=>{console.log(e.target.value);setFontName(e.target.value)}} />
                 <label htmlFor="Krutidev010">Krutidev_010</label>
-            </div>
-            <div>
-                <input className="mr-2" defaultChecked type="radio" id="Krutidev014/016" name="fontName" value="Krutidev014/016"
-                onChange={(e)=>{console.log(e.target.value);setFontName(e.target.value)}} />
-                <label htmlFor="Krutidev014/016">Krutidev_014/016/...</label>
             </div>
           </div>
         </div>
@@ -65,10 +65,10 @@ export function CardWithForm() {
           
           {/* textarea for krutidev */}
           
-          <div className="grid md:col-span-4  border border-y-0 border-purple-400">
+          <div className="grid md:col-span-4  border border-y-0 border-gray-400">
             <div>
                 <div className="">
-                {fontName === "Krutidev014/016"? <Textarea id="outputDiv" className="font-kruti text-2xl/7 text-white  bg-purple-500"
+                {fontName === "Krutidev014/016"? <Textarea id="outputDiv" className="font-kruti text-2xl/7 text-black bg-white"
                   value={krutidevText}
                   onChange={(e: any) => {
                     setKrutidevText(e.target.value);
@@ -76,7 +76,7 @@ export function CardWithForm() {
                   placeholder=";gk¡ Øqrhnso VsDLV isLV@Vkbi djsa---"
                 />:
 
-                <Textarea id="outputDiv" className="font-kruti_010 text-2xl/7 text-white  bg-purple-500"
+                <Textarea id="outputDiv" className="font-kruti_010 text-2xl/7 text-black  bg-white"
                   value={krutidevText}
                   onChange={(e: any) => {
                     setKrutidevText(e.target.value);
@@ -85,7 +85,7 @@ export function CardWithForm() {
                 />}
               </div>
             </div>
-            <div className="grid grid-cols-2  px-20 gap-4 py-2 bg-purple-500 border-0">
+            <div className="grid grid-cols-2  px-20 gap-4 py-2 bg-white border-0">
               <Button onClick={handlePaste} variant={"newButton"}>Paste</Button>
               <Button onClick={()=>{
                 setCopyStatus(false)
@@ -98,30 +98,13 @@ export function CardWithForm() {
           
 
           {/* Button & loader for conversion action */}
-          <div className="md:col-span-2 grid bg-purple-500 justify-center items-center ">            
+          <div className="md:col-span-2 grid bg-white justify-center items-center ">            
            
              <Button
               variant={"outline"}
               onClick={()=>fontName==="Krutidev014/016"?convert_to_unicode(krutidevText, setUnicodeText, setCopyStatus):convert_to_unicode_010 (krutidevText, setUnicodeText, setCopyStatus)}
-              
-              //   async (e) => {
-              //   e.preventDefault();
-              //   setLoading(true)
-              //   await axios
-              //     .post(`${import.meta.env.VITE_BACKEND_URL}/api/convert`,{ krutidevText })
-              //     .then((Response) => {
-              //       setUnicodeText(Response.data.unicodeText);
-              //       setLoading(false)
-              //     })
-              //     .catch((error) => console.log(error));
-              // }
-              // ()=>{
-              //   const convertedText = convert_to_unicode(krutidevText)
-              //   setUnicodeText(convertedText)
-              // }
-              // }
               >
-              Convert  
+              <span className="p-5 text-xl">Convert</span>  
               </Button>
               
           </div>
@@ -131,14 +114,14 @@ export function CardWithForm() {
                   <Textarea className="text-xl" readOnly  value={unicodeText} />
                 </div> */}
 
-                <div className="grid md:col-span-4  border border-y-0 border-purple-400">
+                <div className="grid md:col-span-4  border border-y-0 border-gray-400">
             <div>
                 <div className="">
-                <Textarea id="unic" className="text-base text-white bg-purple-500" readOnly  value={unicodeText} />
+                <Textarea id="unic" className="text-base text-black bg-white" readOnly  value={unicodeText} />
 
               </div>
             </div>
-            <div className="grid px-16 py-2 bg-purple-500 border-0">
+            <div className="grid px-16 py-2 bg-white border-0">
               <Button onClick={handleCopy} variant={"newButton"}>{copyStatus ? <span>Copied &#10004;</span>:<span>Copy</span>}</Button>
             </div>
           </div>
@@ -154,7 +137,7 @@ export function CardWithForm() {
 
         
         {/* Footer starts here */}
-        <div className="bg-gradient-to-r from-purple-500  to-black   rounded-b-md text-center text-gray-400 text-sm md:text-base p-4 font-cursive ">
+        <div className="bg-gradient-to-r from-gray-600 to-black  rounded-b-md text-center text-gray-400 text-sm md:text-base p-4 font-cursive ">
           Copyright &copy; 2025. All rights reserved.
         </div>
       
